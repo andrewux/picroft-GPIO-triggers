929096c665f2bc9eb0482f4763d40f29fba0564e
Andrew Martin <andrewux@users.noreply.github.com>
2022-09-27 21:27:51 +0100

    Update __init__.py
    Created multiple control outputs on pins 13, 19, and 26
    Removed PWM controls

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 """
 skill picroft-led
 Copyright (C) 2021 Andreas Rother
+Modified for multiple outputs by Andrew Martin 2022
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -19,9 +20,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from mycroft import MycroftSkill
 from mycroft.messagebus.message import Message
 
-from gpiozero import PWMLED
+from gpiozero import DigitalOutputDevice
 
-led = PWMLED(12)
+outA = DigitalOutputDevice(13)
+outB = DigitalOutputDevice(19)
+outC = DigitalOutputDevice(26)
 
 class PicroftLED(MycroftSkill):
     def __init__(self):
@@ -29,8 +32,8 @@ class PicroftLED(MycroftSkill):
     
     def initialize(self):
         try:
-            led.blink(n=1)           
-        except led.error:
+            outA.blink(n=1)           
+        except outA.error:
             self.log.warning("Can't initialize GPIO - skill will not load")
             self.speak_dialog("error.initialise")
         finally:    
@@ -44,36 +47,38 @@ class PicroftLED(MycroftSkill):
                            self.handler_audio_output_start)
             self.add_event('recognizer_loop:audio_output_end',
                            self.handler_audio_output_end)
-            self.add_event('recognizer_loop:wakeword',
-                          self.handler_wakeword)
+           # self.add_event('recognizer_loop:wakeword',
+                         # self.handler_wakeword)
             self.add_event('mycroft.stop',
                            self.handler_mycroft_stop)
 
     def handler_mycroft_stop(self, message):
         # code to excecute when mycroft.stop message detected...
-        led.off()
+        outA.off()
+        outB.off()
+        outC.off()
             
     def handle_listener_started(self, message):
         # code to excecute when active listening begins...
         # light up led
-        led.on()
+        outA.on()
 
     def handle_listener_ended(self, message):
-        led.off()
+        outA.off()
     
     def handler_utterance(self, message):
-        led.blink(n=5)
+        outB.on()
     
     def handler_audio_output_start(self, message):
         # start fading led
-        led.pulse()
+        outC.on()
         
     def handler_audio_output_end(self, message):
         # stop fading led
-        led.off()
-    def handler_wakeword(self, message):
+        outC.off()
+   # def handler_wakeword(self, message):
         # code to excecute when recognizer_loop:wakeword message
-        led.pulse(fade_in_time=0.5, fade_out_time=0.5, n=1)
+       # ledA.pulse(fade_in_time=0.5, fade_out_time=0.5, n=1)
         
 def create_skill():
-    return PicroftLED()
+    return PicroftCASELIGHTS()
